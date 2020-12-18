71542cb5f4ff20cadca6c55a7ac2939933c149f3
kSotin <57791410+kSotin@users.noreply.github.com>
2020-12-18 16:52:21 +0800

    add timeout

--- cleartorrents.py
+++ cleartorrents.py
@@ -5,15 +5,15 @@ from credentials import *
 def main():
     r_auth = requests.post('http://localhost:8080/api/v2/auth/login', \
              headers={'Referer': 'http://localhost:8080'}, \
-             data={'username': qbtusername, 'password': qbtpassword})
+             data={'username': qbtusername, 'password': qbtpassword}, timeout=27.05)
     r_get = requests.get('http://localhost:8080/api/v2/torrents/info?filter=completed&sort=completion_on', \
-            cookies=r_auth.cookies.get_dict())
+            cookies=r_auth.cookies.get_dict(), timeout=27.05)
     if r_get.json():
         for torrent in r_get.json():
             if torrent['max_seeding_time'] == max_seeding_time and not 'paused' in torrent['state']:
                 print('[Working] Pausing torrent "' + torrent['name'] + '"...')
                 r_pause = requests.get('http://localhost:8080/api/v2/torrents/pause?hashes=' + torrent['hash'], \
-                          cookies=r_auth.cookies.get_dict())
+                          cookies=r_auth.cookies.get_dict(), timeout=27.05)
                 if r_pause.status_code == 200:
                     print('[Working] Torrent "' + torrent['name'] + '" paused.')
                     exit()
