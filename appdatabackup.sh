faa108ff8b2cebbfc77c005e47d13e74d5b7f1ce
kSotin <57791410+kSotin@users.noreply.github.com>
2021-01-08 23:08:47 +0800

    disable bazarr backup

--- appdatabackup.sh
+++ appdatabackup.sh
@@ -4,11 +4,9 @@ set -e
 PLEX_LOCAL="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
 RADARR_LOCAL=/home/ruxn/.config/Radarr/Backups/scheduled
 SONARR_LOCAL=/var/lib/sonarr/Backups/scheduled
-BAZARR_LOCAL=/opt/bazarr/data
 PLEX_CLOUD=gdrive:backups/plexbackups
 RADARR_CLOUD=gdrive:backups/radarrbackups
 SONARR_CLOUD=gdrive:backups/sonarrbackups
-BACKUPS_CLOUD=gdrive:backups
 
 if [ -d "${PLEX_LOCAL}" -a "$(ls "${PLEX_LOCAL}")" ]
 then
@@ -32,16 +30,5 @@ then
     /usr/bin/rclone sync "${SONARR_LOCAL}" "${SONARR_CLOUD}" --log-level INFO
 else
     echo "No archives found in local SONARR backup directory, skipping..."
-    echo
-fi
-if [ -d "${BAZARR_LOCAL}" ]
-then
-    echo "Creating backup archive for BAZARR..."
-    sudo tar -cf /home/ruxn/bazarrbackup.tar -C "${BAZARR_LOCAL}" .
-    echo "Backup archive created for BAZARR."
-    echo "Uploading to cloud..."
-    /usr/bin/rclone move /home/ruxn/bazarrbackup.tar "${BACKUPS_CLOUD}" --log-level INFO
-else
-    echo "BAZARR appdata directory not found, skipping..."
     echo
 fi
