c6ca7608ce17a2e967bbcf5ef7e53da5cd0b1d0e
kSotin <57791410+kSotin@users.noreply.github.com>
2021-01-26 20:09:50 +0800

    redesign strategy

--- statuscheck.py
+++ statuscheck.py
@@ -11,88 +11,106 @@ import subprocess
 
 
 # For status syncing
-def send_statuses(statuses, sender_port, receiver_addr, receiver_port):
+def send_statuses(sender_port, receiver_addr, receiver_port):
     data = json.dumps(statuses)
     sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sender_socket.bind(('', sender_port))
     sender_socket.sendto(data.encode(), (receiver_addr, receiver_port))
-    print('[Syncing] Sent statuses to (' + str(receiver_addr) + ', ' + str(receiver_port) + ').')
+    # print('[Syncing] Sent statuses to (' + str(receiver_addr) + ', ' + str(receiver_port) + ').')
 
 
-def receive_statuses(receiver_port, list, index, sender_name, isalive):
+def receive_statuses(receiver_port, index, sender_name):
     receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     receiver_socket.bind(('', receiver_port))
-    alivechecker = threading.Thread(target=receive_timeout_reset, args=(list, index, sender_name, isalive))
+    alivechecker = threading.Thread(target=receive_timeout_reset, args=(index, sender_name))
     alivechecker.start()
     while True:
         data, sender_addr = receiver_socket.recvfrom(2048)
         isalive[index] = True
-        print('[Syncing] Received statuses from ' + str(sender_addr) + '.')
-        list[index] = json.loads(data.decode())
+        # print('[Syncing] Received statuses from ' + str(sender_addr) + '.')
+        listofstatuses[index] = json.loads(data.decode())
 
 
-def receive_timeout_reset(list, index, sender_name, isalive):
+def receive_timeout_reset(index, sender_name):
     clock = 0
     while True:
         if isalive[index]:
             isalive[index] = False
             clock = 0
-        if clock == 600:
+        if clock == 250:
             print('[Checker Death] ' + sender_name.upper() + ' is dead.')
-            for component in list[index]:
-                list[index][component] = True
+            listofstatuses[index] = None
             isalive[index] = False
             clock = 0
         time.sleep(1)
         clock += 1
 
 
-def merge_statuses(merge_to, merge_from):
-    for statuses in merge_from:
-        for component in statuses:
-            merge_to[component] = merge_to[component] and statuses[component]
+def check_consistency(component):
+    component_status = []
+    for statuses in listofstatuses:
+        if statuses != None:
+            if component in statuses:
+                component_status.append(statuses[component])
+    print(component_status)
+    if len(set(component_status)) == 1:
+        return True
+    else:
+        return False
+
+
+def announce(status, component):
+    while statuses[component] == status:
+        if check_consistency(component):
+            if statuses[component]:
+                if component in outage:
+                    print('[Announcing] Announcing restoration of ' + component.title() + '.')
+                    r = requests.patch('https://api.statuspage.io/v1/pages/' + page_id + \
+                        '/components/' + components_id[component], \
+                        headers = {'Authorization': 'OAuth ' + API_key}, \
+                        data = json.dumps({"component": {"status": "operational"}}), timeout=27.05)
+                    outage.remove(component)
+            else:
+                print('[Announcing] Announcing outage of ' + component.title() + '.')
+                r = requests.patch('https://api.statuspage.io/v1/pages/' + page_id + \
+                    '/components/' + components_id[component], \
+                    headers = {'Authorization': 'OAuth ' + API_key}, \
+                    data = json.dumps({"component": {"status": "major_outage"}}), timeout=27.05)
+                outage.add(component)
+            break
+        time.sleep(10)
 
 
 # For status checking
-
-def check_site(site_url, isDown):
-    i = 0
-    while i < 3:
-        try:
-            r = requests.get(site_url, timeout=27.05)
-        except requests.exceptions.RequestException:
-            if isDown:
-                return False
-            i += 1
+def check_site(site_url):
+    try:
+        r = requests.get(site_url, timeout=27.05)
+    except requests.exceptions.RequestException:
+        return False
+    else:
+        if r.status_code == 200:
+            return True
         else:
-            if r.status_code == 200:
-                return True
-            else:
-                return False
-    return False
+            return False
 
 
-def check_proxy(proxy_url, isDown):
+def check_proxy(proxy_url):
     s = requests.Session()
     s.mount('https://', host_header_ssl.HostHeaderSSLAdapter())
-    i = 0
-    while i < 3:
-        try:
-            r = s.get('https://' + proxy_url + '/statuscheck', headers={'Host': 'plex.ruxn.media'}, timeout=27.05)
-        except requests.exceptions.RequestException:
-            if isDown:
-                return False
-            i += 1
+    try:
+        r = s.get('https://' + proxy_url + '/statuscheck', headers={'Host': 'plex.ruxn.media'}, timeout=27.05)
+    except requests.exceptions.RequestException:
+        return False
+    else:
+        if r.status_code == 200:
+            return True
         else:
-            if r.status_code == 200:
-                return True
-            else:
-                return False
-    return False
+            return False
 
 
 def check_service(service):
-    return_code = subprocess.call('systemctl is-active ' + service + '.service', stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
+    return_code = subprocess.call('systemctl is-active ' + service + '.service', \
+        stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
     if return_code == 0:
         return True
     else:
@@ -100,8 +118,10 @@ def check_service(service):
 
 
 def check_timer(timer):
-    timer_active = subprocess.call('systemctl is-active ' + timer + '.timer', stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
-    service_failed = subprocess.call('systemctl is-failed ' + timer + '.service', stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
+    timer_active = subprocess.call('systemctl is-active ' + timer + '.timer', \
+        stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
+    service_failed = subprocess.call('systemctl is-failed ' + timer + '.service', \
+        stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True)
     if timer_active == 0 and service_failed != 0:
         return True
     else:
@@ -117,144 +137,122 @@ def fetch_from_page():
     return statuses
 
 
-def announce_outage(component_id):
-    r = requests.patch('https://api.statuspage.io/v1/pages/' + page_id + '/components/' + component_id, \
-        headers = {'Authorization': 'OAuth ' + API_key}, \
-        data = json.dumps({"component": {"status": "major_outage"}}), timeout=27.05)
+# main
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "t:", ["tier="])
+except getopt.GetoptError:
+    print('Error: ' + sys.argv[0] + ' -t <tier>')
+    print('       ' + sys.argv[0] + ' --tier=<tier>')
+    sys.exit(2)
+else:
+    for opt, arg in opts:
+        if opt in ("-t", "--tier"):
+            tier = int(arg)
 
+# Initialize statuses
+status_trans = {
+    'operational': True,
+    'major_outage': False,
+}
+init_statuses = fetch_from_page()
+for component in statuses:
+    statuses[component] = status_trans.get(init_statuses[components_id[component]], True)
+listofstatuses = [statuses]
+consistency = statuses.copy()
+outage = set()
 
-def announce_restoration(to_announce, listofstatuses):
-    time.sleep(30)
-    lock.acquire()
-    try:
-        merge_statuses(statuses, listofstatuses[1:])
-        for component in list(to_announce):
-            if statuses[component]:
-                print('[Announcing] Announcing restoration of ' + component.title() + '.')
-                r = requests.patch('https://api.statuspage.io/v1/pages/' + page_id + '/components/' + components_id[component], \
-                    headers = {'Authorization': 'OAuth ' + API_key}, \
-                    data = json.dumps({"component": {"status": "operational"}}), timeout=27.05)
-                to_announce.remove(component)
-    finally:
-        lock.release()
+# Sync statuses - receiver
+if tier != 0:
+    listofthreads = []
+    isalive = [True]
+    indexofstatuses = 1
+    for checker in sync_from_port:
+        listofstatuses.append(None)
+        listofthreads.append(threading.Thread(target=receive_statuses, name=checker, \
+            args=(sync_from_port[checker], indexofstatuses, checker)))
+        listofthreads[-1].start()
+        isalive.append(False)
+        indexofstatuses += 1
 
-
-lock = threading.Lock()
-
-
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv[1:], "t:", ["tier="])
-    except getopt.GetoptError:
-        print('Error: ' + argv[0] + ' -t <tier>')
-        print('       ' + argv[0] + ' --tier=<tier>')
-        sys.exit(2)
-    else:
-        for opt, arg in opts:
-            if opt in ("-t", "--tier"):
-                tier = int(arg)
-
-    # Initialize statuses
-    status_trans = {
-        'operational': True,
-        'major_outage': False,
-    }
-    init_statuses = fetch_from_page()
-    for component in statuses:
-        statuses[component] = status_trans.get(init_statuses[components_id[component]], True)
-    to_announce = set()
-    listofstatuses = [statuses]
-
-    # Sync statuses - receiver
-    if tier != 0:
-        listofthreads = []
-        isalive = [True]
-        indexofstatuses = 1
-        for checker in sync_from_port:
-            listofstatuses.append(statuses.copy())
-            listofthreads.append(threading.Thread(target=receive_statuses, name=checker, args=(sync_from_port[checker], listofstatuses, indexofstatuses, checker, isalive)))
-            listofthreads[-1].start()
-            isalive.append(False)
-            indexofstatuses += 1
-
-    while True:
-        # Host
-        if tier == 0:
-            # Check services
-            for service in services:
-                if not check_service(service) and statuses[service]:
+while True:
+    # Host
+    if tier == 0:
+        # Check services
+        for service in services:
+            if statuses[service]:
+                if not check_service(service):
                     print('[Outage] An outage detected of ' + service + '.')
-                    announce_outage(components_id[service])
                     statuses[service] = False
-                if check_service(service) and not statuses[service]:
+                    announcer = threading.Thread(target=announce, args=(False, service))
+                    announcer.start()
+            else:
+                if check_service(service):
                     print('[Restoration] Restoration from outage detected of ' + service + '.')
                     statuses[service] = True
-                    to_announce.add(service)
-            # Check timers
-            for timer in timers:
-                if not check_timer(timer) and statuses[timer]:
+                    announcer = threading.Thread(target=announce, args=(True, service))
+                    announcer.start()
+        # Check timers
+        for timer in timers:
+            if statuses[timer]:
+                if not check_timer(timer):
                     print('[Outage] An outage detected of ' + timer + '.')
-                    announce_outage(components_id[timer])
                     statuses[timer] = False
-                if check_timer(timer) and not statuses[timer]:
+                    announcer = threading.Thread(target=announce, args=(False, timer))
+                    announcer.start()
+            else:
+                if check_timer(timer):
                     print('[Restoration] Restoration from outage detected of ' + timer + '.')
                     statuses[timer] = True
-                    to_announce.add(timer)
-        # Tier-1 proxy
-        elif tier == 1:
-            # Check sites
-            for site in sites_url:
-                if statuses[site]:
-                    if not check_site(sites_url[site], False):
-                        print('[Outage] An outage detected of ' + site.title() + '.')
-                        announce_outage(components_id[site])
-                        statuses[site] = False
-                else:
-                    if check_site(sites_url[site], True):
-                        print('[Restoration] Restoration from outage detected of ' + site.title() + '.')
-                        statuses[site] = True
-                        to_announce.add(site)
-            # Check proxies
-            for proxy in proxies_url:
-                if statuses[proxy]:
-                    if not check_proxy(proxies_url[proxy], False):
-                        print('[Outage] An outage detected of ' + proxy.title() + '.')
-                        announce_outage(components_id[proxy])
-                        statuses[proxy] = False
-                else:
-                    if check_proxy(proxies_url[proxy], True):
-                        print('[Restoration] Restoration from outage detected of ' + proxy.title() + '.')
-                        statuses[proxy] = True
-                        to_announce.add(proxy)
-        else:
-            # Check proxies
-            for proxy in proxies_url:
-                if statuses[proxy]:
-                    if not check_proxy(proxies_url[proxy], False):
-                        print('[Outage] An outage detected of ' + proxy.title() + '.')
-                        announce_outage(components_id[proxy])
-                        statuses[proxy] = False
-                else:
-                    if check_proxy(proxies_url[proxy], True):
-                        print('[Restoration] Restoration from outage detected of ' + proxy.title() + '.')
-                        statuses[proxy] = True
-                        to_announce.add(proxy)
-        # Reset statuses
-        if 'skip' in locals() or 'skip' in globals():
-            for component in skip:
-                statuses[component] = True
-        # Sync statuses - sender
-        if tier != 0:
-            for checker in sync_receivers_port:
-                send_statuses(statuses, sync_sender_port, checkers_addr[checker], sync_receivers_port[checker])
-                # time.sleep(5)
-                # send_statuses(statuses, sync_sender_port, checkers_addr[checker], sync_receivers_port[checker])
-        # Announce restoration
-        announcer = threading.Thread(target=announce_restoration, args=(to_announce, listofstatuses))
-        announcer.start()
+                    announcer = threading.Thread(target=announce, args=(True, timer))
+                    announcer.start()
+    # Tier-1 proxy
+    elif tier == 1:
+        # Check sites
+        for site in sites_url:
+            if statuses[site]:
+                if not check_site(sites_url[site]):
+                    print('[Outage] An outage detected of ' + site.title() + '.')
+                    statuses[site] = False
+                    announcer = threading.Thread(target=announce, args=(False, site))
+                    announcer.start()
+            else:
+                if check_site(sites_url[site]):
+                    print('[Restoration] Restoration from outage detected of ' + site.title() + '.')
+                    statuses[site] = True
+                    announcer = threading.Thread(target=announce, args=(True, site))
+                    announcer.start()
+        # Check proxies
+        for proxy in proxies_url:
+            if statuses[proxy]:
+                if not check_proxy(proxies_url[proxy]):
+                    print('[Outage] An outage detected of ' + proxy.title() + '.')
+                    statuses[proxy] = False
+                    announcer = threading.Thread(target=announce, args=(False, proxy))
+                    announcer.start()
+            else:
+                if check_proxy(proxies_url[proxy]):
+                    print('[Restoration] Restoration from outage detected of ' + proxy.title() + '.')
+                    statuses[proxy] = True
+                    announcer = threading.Thread(target=announce, args=(True, proxy))
+                    announcer.start()
+    else:
+        # Check proxies
+        for proxy in proxies_url:
+            if statuses[proxy]:
+                if not check_proxy(proxies_url[proxy]):
+                    print('[Outage] An outage detected of ' + proxy.title() + '.')
+                    statuses[proxy] = False
+                    announcer = threading.Thread(target=announce, args=(False, proxy))
+                    announcer.start()
+            else:
+                if check_proxy(proxies_url[proxy]):
+                    print('[Restoration] Restoration from outage detected of ' + proxy.title() + '.')
+                    statuses[proxy] = True
+                    announcer = threading.Thread(target=announce, args=(True, proxy))
+                    announcer.start()
+    # Sync statuses - sender
+    if tier != 0:
+        for checker in sync_receivers_port:
+            send_statuses(sync_sender_port, checkers_addr[checker], sync_receivers_port[checker])
 
-        time.sleep(15)
-
-
-if __name__ == "__main__":
-   main(sys.argv)
+    time.sleep(15)
--- statuspropagate.py
+++ statuspropagate.py
@@ -5,6 +5,7 @@ import json
 import sys
 import getopt
 import subprocess
+import os
 
 
 def fetch_from_page():
@@ -24,6 +25,20 @@ def IFTTT_announce(component_name, new_status):
         timeout=27.05)
 
 
+def read_inwall_proxy():
+    headers = {
+        'X-Auth-Email': cloudflare_email,
+        'X-Auth-Key': cloudflare_key,
+        'Content-Type': 'application/json',
+    }
+    r = requests.get('https://api.cloudflare.com/client/v4/zones/' + cloudflare_zoneid + \
+        '/dns_records/' + cloudflare_recordid, headers=headers, timeout=27.05)
+    if r.json()["result"]["content"] == inwall_main_ip:
+        return 1
+    if r.json()["result"]["content"] == inwall_backup_ip:
+        return 2
+
+
 def switch_inwall_proxy(to_backup):
     headers = {
         'X-Auth-Email': cloudflare_email,
@@ -35,7 +50,16 @@ def switch_inwall_proxy(to_backup):
     else:
         data = '{"content":"' + inwall_main_ip + '"}'
     r = requests.patch('https://api.cloudflare.com/client/v4/zones/' + cloudflare_zoneid + \
-        '/dns_records/' + cloudflare_recordid , headers=headers, data=data, timeout=27.05)
+        '/dns_records/' + cloudflare_recordid, headers=headers, data=data, timeout=27.05)
+
+
+def read_nginx_config():
+    if os.path.exists('/etc/nginx/sites-enabled/plex'):
+        return 1
+    if os.path.exists('/etc/nginx/sites-enabled/plex_2'):
+        return 2
+    if os.path.exists('/etc/nginx/sites-enabled/plex_3'):
+        return 3
 
 
 def switch_nginx_config(to_config):
@@ -65,74 +89,81 @@ def switch_nginx_config(to_config):
     return True
 
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv[1:], "t:", ["tier="])
-    except getopt.GetoptError:
-        print('Error: ' + argv[0] + ' -t <tier>')
-        print('       ' + argv[0] + ' --tier=<tier>')
-        sys.exit(2)
-    else:
-        for opt, arg in opts:
-            if opt in ("-t", "--tier"):
-                tier = int(arg)
+# main
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "t:", ["tier="])
+except getopt.GetoptError:
+    print('Error: ' + sys.argv[0] + ' -t <tier>')
+    print('       ' + sys.argv[0] + ' --tier=<tier>')
+    sys.exit(2)
+else:
+    for opt, arg in opts:
+        if opt in ("-t", "--tier"):
+            tier = int(arg)
 
-    # Initialization
-    diff = statuses_full.copy()
-    page_info = fetch_from_page()
-    components_name = page_info[1]
+# Initialization
+diff = statuses_full.copy()
+page_info = fetch_from_page()
+components_name = page_info[1]
+for component in statuses_full:
+    statuses_full[component] = page_info[0].get(components_id[component], 'operational')
+    diff[component] = False
+new_statuses = statuses_full.copy()
+
+while True:
+    statuses = new_statuses.copy()
+    new_page_info = fetch_from_page()
     for component in statuses_full:
-        statuses_full[component] = page_info[0].get(components_id[component], 'operational')
-        diff[component] = False
-    new_statuses = statuses_full.copy()
+        new_statuses[component] = new_page_info[0].get(components_id[component], 'operational')
+        diff[component] = new_statuses[component] != statuses[component]
 
-    while True:
-        statuses = new_statuses.copy()
-        new_page_info = fetch_from_page()
+    # propagators
+    # Host
+    if tier == 0:
+        # publish to IFTTT
         for component in statuses_full:
-            new_statuses[component] = new_page_info[0].get(components_id[component], 'operational')
-            diff[component] = new_statuses[component] != statuses[component]
+            if diff[component]:
+                IFTTT_announce(components_name[components_id[component]], new_statuses[component])
+                print("[IFTTT] Status update announced of " + component.title() + ": " + \
+                      new_statuses[component] + ".")
 
-        # propagators
-        # Host
-        if tier == 0:
-            # publish to IFTTT
-            for component in statuses_full:
-                if diff[component] == True:
-                    IFTTT_announce(components_name[components_id[component]], new_statuses[component])
-                    print("[IFTTT] Status update announced of " + component.title() + ": " + \
-                          new_statuses[component] + ".")
-
-            # switch to backup in-wall proxy
-            if diff[main_inwall_proxy] == True:
-                if new_statuses[main_inwall_proxy] != 'operational':
+        # switch to backup in-wall proxy
+        if diff[main_inwall_proxy]:
+            if new_statuses[main_inwall_proxy] != 'operational':
+                if read_inwall_proxy() == 1:
                     switch_inwall_proxy(True)
                     print("[Proxy Switch] Switched to backup proxy.")
-                else:
+            else:
+                if read_inwall_proxy() == 2:
                     switch_inwall_proxy(False)
                     print("[Proxy Switch] Switched to main proxy.")
-        # Tier-2 proxy
-        elif tier == 2:
-            if diff[tier1_1] == True:
-                if new_statuses[tier1_1] != 'operational':
-                    if new_statuses[tier1_2] == 'operational':
-                        if switch_nginx_config(2) == True:
-                            print("[Config Switch] Switched to secondary config.")
-                        else:
-                            print("[Config Switch] Config not switched.")
+    # Tier-2 proxy
+    elif tier == 2:
+        if diff[tier1_1]:
+            if new_statuses[tier1_1] != 'operational':
+                if read_nginx_config() == 1:
+                    if switch_nginx_config(2):
+                        print("[Config Switch] Switched to secondary config.")
                     else:
-                        if switch_nginx_config(3) == True:
-                            print("[Config Switch] Switched to tertiary config.")
-                        else:
-                            print("[Config Switch] Config not switched.")
-                else:
-                    if switch_nginx_config(1) == True:
+                        print("[Config Switch] Config not switched.")
+            else:
+                if read_nginx_config() != 1:
+                    if switch_nginx_config(1):
                         print("[Config Switch] Switched to primary config.")
                     else:
                         print("[Config Switch] Config not switched.")
+        if diff[tier1_2]:
+            if new_statuses[tier1_2] != 'operational':
+                if read_nginx_config() == 2:
+                    if switch_nginx_config(3):
+                        print("[Config Switch] Switched to tertiary config.")
+                    else:
+                        print("[Config Switch] Config not switched.")
+            else:
+                if read_nginx_config() != 2:
+                    if switch_nginx_config(2):
+                        print("[Config Switch] Switched to secondary config.")
+                    else:
+                        print("[Config Switch] Config not switched.")
 
-        time.sleep(10)
-
-
-if __name__ == "__main__":
-   main(sys.argv)
+    time.sleep(10)
